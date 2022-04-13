e813ca82e3968184384d124b77a16edb23f48af7
Cypress Wright <48331002+cypressw@users.noreply.github.com>
2022-04-13 18:49:22 -0500

    initial implementation of gamedetails

--- TeamProject/gamedetails/Board.java
+++ TeamProject/gamedetails/Board.java
@@ -1,5 +1,37 @@
 package gamedetails;
 
 public class Board {
+	private Space[][] grid;
+	private Ship[] ships;
+	private int shipCount;
 
+
+	public Board() {
+		grid = new Space[10][10];
+		shipCount = 5;
+		ships = new Ship[shipCount];
+
+	}
+
+	public void setShipCount(int n) {
+		shipCount = n;
+	}
+
+	public int getShipCount() {
+		return shipCount;
+	}
+
+	public void setShips (Ship[] ships) {
+		this.ships = ships;
+
+		// Set the board object in the Ship class
+		for(Ship s : this.ships) {
+			s.setBoard(this);
+		}
+
+	}
+
+	public Ship[] getShips() {
+		return ships;
+	}
 }
--- TeamProject/gamedetails/Ship.java
+++ TeamProject/gamedetails/Ship.java
@@ -8,52 +8,81 @@ public class Ship {
 	private int size;
 	private ArrayList<Space> spacesOccupied;
 	private int health;
-	
+	private Board board;
+
 	public Ship() {
 		name = "unnamed ship";
 		size = 0;
 		spacesOccupied = new ArrayList<Space>();
 		health = size;
 	}
-	
+
 	public void setName(String name) {
 		this.name = name;
 	}
-	
+
 	public void setSize(int size) {
 		this.size = size;
 	}
-	
+
 	public void setHealth(int health) {
 		this.health = health;
 	}
-	
+
 	public void setSpacesOccupied(ArrayList<Space> spaces) {
 		spacesOccupied = spaces;
 	}
-	
+
 	public String getName() {
 		return name;
 	}
-	
+
 	public int getSize() {
 		return size;
 	}
-	
+
 	public ArrayList<Space> getSpacesOccupied(){
 		return spacesOccupied;
 	}
-	
+
 	public int getHealth() {
 		return health;
 	}
-	
-	public void shipHit(int x, int y) {
+
+
+	public Board getBoard() {
+		return board;
+	}
+
+	public void setBoard(Board board) {
+		this.board = board;
+	}
+
+	public void checkShipHit(int x, int y) {	
+
+		// Loop through the spaces occupied by the ship to check for (x,y) guess
 		for (Space s : spacesOccupied) {
-			if (s.getX()==x && s.getY() == y) {
+
+			// If ship occupies guessed space, mark space hit and decrement ship health
+			if (s.getX() == x && s.getY() == y) {
 				s.markHit(true);
+				health--;
+				checkSinkShip();
 				break;
 			}
+		}
+	}
+
+	public void checkSinkShip() {
+
+		// Logic here for what needs to happen if ship sinks
+		// Might not know how we actually want to handle it until more of the game is built
+		if (health == 0) {
+			board.setShipCount(board.getShipCount() - 1);
+			System.out.println("Ship has been sunk.");
+		}
+		else {
+			System.out.println("Ship is still alive.");
 		}
 	}
 }
