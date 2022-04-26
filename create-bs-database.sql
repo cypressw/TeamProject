630da35f48889776f32df59f34026c7d576007be
Cypress Wright <48331002+cypressw@users.noreply.github.com>
2022-04-26 14:42:07 -0500

    final commit

--- create-bs-database.sql
+++
@@ -1,8 +0,0 @@
-CREATE SCHEMA `battleship`;
-
-DROP TABLE `battleship`;
-
-CREATE TABLE `battleship`.`users` (
-  `username` VARCHAR(30) NOT NULL,
-  `password` VARBINARY(64) NOT NULL,
-  PRIMARY KEY (`username`, `password`));
